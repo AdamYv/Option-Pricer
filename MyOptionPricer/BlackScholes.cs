c0303abbcc3873c80be4c66601e7420fda1521a9
AdamYv <adam.guemoune@gmail.com>
2025-02-28 14:23:21 +0100

    Finished Implementation Blash-Scholes

--- MyOptionPricer/BlackScholes.cs
+++ MyOptionPricer/BlackScholes.cs
@@ -1,42 +1,55 @@
 using System;
 using MathNet.Numerics.Distributions;
+namespace MyOptionPricer{
 
 public class BlackSholes {
-    //[ ]: Add the necessary attributes
-    public double S;
-    public double K;
-    public double r;
-    public double sigma;
-    public double T;
 
-    public double call_BS(){
-        float callP=0;
+    //[ ]: Add the necessary attributes 
+    //FIXME On appel deux fois la fonction D_ONE() dans la fonction call_BS() et D_TWO() stocker la valeur apres calcul dans une variable
+    public double S { get; private set; }  
+        public double K { get; private set; }  
+        public double r { get; private set; }  
+        public double sigma { get; private set; }  
+        public double T { get; private set; }  
 
-        return callP;
+
+    public BlackSholes(double S, double K, double r, double sigma, double T){
+        this.S = S;
+        this.K = K;
+        this.r = r;
+        this.sigma = sigma;
+        this.T = T;
+    }
+
+    public double Call_BS(){
+        // Compute D1 and D2
+        double sqrT = Math.Sqrt(T);
+        double d1 = (Math.Log(S /K)+(r+sigma*sigma/2.0)*T) /sigma * sqrT;
+
+        double d2 = d1 - sigma * sqrT;
+
+        //For the normal Law we use N(0,1, x) to get the cumulative distribution function 
+        double N_d1 = Normal.CDF(0.0, 1.0, d1);
+        double N_d2 = Normal.CDF(00, 1.0, d2);
+
+        return S * N_d1 - K * Math.Exp(- r * T) * N_d2;
         
     }
 
-    public double put_BS(){
-        // Black-Scholes formula for put option
-        return 0;
+    public double Put_BS(){
+    double sqrT = Math.Sqrt(T);
+    double d1 = (Math.Log(S /K)+(r+sigma*sigma/2.0)*T)/sigma * sqrT;
+
+    double d2 = d1 - sigma * sqrT;
+
+    double N_d1 = Normal.CDF(0.0, 1.0, -d1);
+    double N_d2 = Normal.CDF(00, 1.0, -d2);
+
+    return K * Math.Exp(- r * T) * N_d2 - S * N_d1;
     }
-
-    public double Function_Cumul(){
-        // Cumulative distribution functionattributes
-
-        return 0;
-    }
-
-    public double D_ONE(){
-        double d1 = Math.Log(this.S, this.K) + (this.r + Math.Pow(this.sigma, 2) / 2) * this.T;
-        d1 /= this.sigma * Math.Sqrt(this.T);
-        return d1;
-    }
-
-    public double D_TWO(){
-        double d2 = this.D_ONE() - this.sigma * Math.Sqrt(this.T);
-        return d2;
-    }
+    
 
 
+
+}
 }
--- MyOptionPricer/Program.cs
+++ MyOptionPricer/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using MathNet.Numerics.Distributions;
 
 namespace MyOptionPricer
 {
@@ -7,12 +6,25 @@ namespace MyOptionPricer
     {
         static void Main(string[] args)
         {
-        double mean = 0.0; // Moyenne (mu)
-        double stdDev = 1.0; // Écart-type (sigma)
-            double x = 1.0; // Valeur pour laquelle on veut la CDF
+            Console.WriteLine("Bienvenue dans le calculateur de prix d'options Black-Scholes !");
 
-        double cdf = Normal.CDF(mean, stdDev, x);
-        Console.WriteLine($"La CDF de N({mean}, {stdDev}²) pour x = {x} est : {cdf}");
+            // Paramètres de l'option
+            double spotPrice = 100.0;       // Prix actuel de l'actif sous-jacent (S)
+            double strikePrice = 95.0;      // Prix d'exercice (K)
+            double riskFreeRate = 0.05;     // Taux sans risque (r)
+            double volatility = 0.2;        // Volatilité (sigma)
+            double timeToMaturity = 1.0;    // Temps jusqu'à l'expiration en années (T)
+
+            // Création de l'objet BlackSholes
+            BlackSholes black = new BlackSholes(spotPrice, strikePrice, riskFreeRate, volatility, timeToMaturity);
+
+            // Calcul du prix de l'option call
+            double callPrice = black.Call_BS();
+            Console.WriteLine($"Prix de l'option call : {callPrice:F4}");
+
+            // Calcul du prix de l'option put
+            double putPrice = black.Put_BS();
+            Console.WriteLine($"Prix de l'option put : {putPrice:F4}");
         }
     }
 }
